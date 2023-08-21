8f01f96e57373b90bc4a7aec7ada0b8097d82e64
Kirill Kurentsov <immortalsteel@gmail.com>
2023-08-21 17:48:47 +0300

    feat: Добавлена сортировка, очистка списка, сохранение в LS

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-    <h2>Список продуктов</h2>
-    <ul>
+  <div
+    style="
+      margin: 0 auto;
+      max-width: 280px;
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+    "
+  >
+    <h2 style="margin: 0; text-align: center">Список продуктов</h2>
+    <ul
+      style="
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+      "
+    >
       <li
         v-for="(product, index) in products"
         :key="index"
@@ -12,16 +29,21 @@
       </li>
     </ul>
     <AddProduct @addProduct="addProduct" />
+    <ClearListComponent @clearProductList="clearProductList" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import AddProduct from "./AddProduct.vue";
+import ClearListComponent from "./ClearListComponent.vue";
+
+const localStorage = window.localStorage;
 
 export default defineComponent({
   components: {
     AddProduct,
+    ClearListComponent,
   },
   data() {
     return {
@@ -31,11 +53,45 @@ export default defineComponent({
   methods: {
     addProduct(newProduct: string) {
       this.products.push({ name: newProduct, crossedOut: false });
+      this.sortProducts();
     },
     crossOutProduct(index: number) {
       this.products[index].crossedOut = !this.products[index].crossedOut;
-      const crossedProduct = this.products.splice(index, 1)[0];
-      this.products.push(crossedProduct);
+      this.sortProducts();
+    },
+
+    sortProducts() {
+      this.products.sort((a, b) => {
+        if (a.crossedOut === b.crossedOut) {
+          return 0;
+        }
+        if (a.crossedOut) {
+          return 1;
+        }
+        return -1;
+      });
+    },
+
+    saveState() {
+      localStorage.setItem("shoppingList", JSON.stringify(this.products));
+    },
+    loadState() {
+      const savedState = localStorage.getItem("shoppingList");
+      if (savedState) {
+        this.products = JSON.parse(savedState);
+      }
+    },
+    clearProductList() {
+      this.products = [];
+    },
+  },
+  created() {
+    this.loadState();
+  },
+  watch: {
+    products: {
+      handler: "saveState",
+      deep: true,
     },
   },
 });
